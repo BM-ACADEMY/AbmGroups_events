a84bb5533f4d818420624dd138e09a75745b3872
BM-ACADEMY <teamkamarweb@gmail.com>
2025-10-13 22:06:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -92,5 +92,5 @@ app.use("/api/pattern-marks", require("./routes/patternMarksRoutes"));
 connectDB();
 
 // ====== Server ======
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
